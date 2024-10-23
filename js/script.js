c9110bc70e2aa672a415eddbe5fa2cbadce758cd
AnaClaraRivas <anaclararivasbronzeri@gmail.com>
2024-10-23 08:52:23 -0300

    menu/footer/sections mudaanças

--- js/script.js
+++ js/script.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("resize", adjustPadding);
   });
   
+document.addEventListener('scroll', () => {
+    const footer = document.querySelector('footer');
+    const sections = document.querySelectorAll('section');
+    
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+            footer.style.backgroundColor = getComputedStyle(section).backgroundColor;
+        }
+    });
+});
+
+document.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.nav-bar-color');
+  const sections = document.querySelectorAll('section');
+
+  sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      // Verifica se a seção está completamente visível
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          navbar.style.backgroundColor = getComputedStyle(section).backgroundColor;
+      }
+  });
+});
